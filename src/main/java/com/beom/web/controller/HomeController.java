c643d6fc268c9aa78d4c766d429e94fb3ad2f2ab
beomjunpark <pbeomjun12@gmail.com>
2023-06-24 01:55:58 +0900

    /, 공통 css수정

--- src/main/java/com/beom/web/controller/HomeController.java
+++ src/main/java/com/beom/web/controller/HomeController.java
@@ -1,24 +1,15 @@
 package com.beom.web.controller;
 
-import com.beom.web.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @Slf4j
 public class HomeController {
 
-//    @GetMapping("/")
-//    public String main() {
-//        log.info("HomeController");
-//        return "home";
-//    }
-
     @GetMapping("/")
-    public String loginHome() {
+    public String Home() {
         return "home";
     }
 }
